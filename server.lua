3756fda469938c5db60c881badd629984a66a28a
Jason Hines <jason@greenhell.com>
2020-02-20 01:27:05 -0500

    add safety warning to log

--- server.lua
+++ server.lua
@@ -1,3 +1,10 @@
+AddEvent("OnPackageStart", function()
+    print "*********************************************"
+    print " WARNING: Ingame editor package is running!  "
+    print " DO NOT RUN THIS PACKAGE ON A PUBLIC SERVER! "
+    print "*********************************************"
+end)
+
 AddRemoteEvent("ingame_editor:RunServer", function(player, code)
     if code == "" then return end
 
